79e67373a736d1a608e4a406ecaee5c30b68fe5a
ertzu <ertzu@LAPTOP-80KKL9FG>
2022-03-03 10:09:36 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			Előre(10); 
+		}
+	}
+}
